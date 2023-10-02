dc8442d8dfa4279103508b0949ddd24be5a408a8
Guilherme <guilherme_fcm@hotmail.com>
2023-10-02 20:30:48 -0300

    Toast Capture screen data

--- app/src/main/java/com/example/luckyapp/ui/capture/CaptureFragment.java
+++ app/src/main/java/com/example/luckyapp/ui/capture/CaptureFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -16,17 +17,24 @@ public class CaptureFragment extends Fragment {
 
     private FragmentCaptureBinding binding;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(
+            @NonNull LayoutInflater inflater,
+            ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        binding = FragmentCaptureBinding.inflate(inflater, container, false);
+
         CaptureViewModel captureViewModel =
                 new ViewModelProvider(this).get(CaptureViewModel.class);
 
-        binding = FragmentCaptureBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        captureViewModel.getData().observe(getViewLifecycleOwner(), data -> {
+            Toast.makeText(getContext(), captureViewModel.join(), Toast.LENGTH_SHORT).show();
+            Toast.makeText(getContext(), captureViewModel.major().toString(), Toast.LENGTH_SHORT).show();
+            Toast.makeText(getContext(), captureViewModel.average().toString(), Toast.LENGTH_SHORT).show();
+            Toast.makeText(getContext(), captureViewModel.descending().toString(), Toast.LENGTH_SHORT).show();
+        });
 
-        final TextView textView = binding.textSlideshow;
-        captureViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        return binding.getRoot();
     }
 
     @Override
--- app/src/main/java/com/example/luckyapp/ui/capture/CaptureViewModel.java
+++ app/src/main/java/com/example/luckyapp/ui/capture/CaptureViewModel.java
@@ -1,19 +1,73 @@
 package com.example.luckyapp.ui.capture;
 
+import android.os.Handler;
+import android.util.Log;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.luckyapp.utils.JsonDataService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
+
 public class CaptureViewModel extends ViewModel {
 
-    private final MutableLiveData<String> mText;
+    private final MutableLiveData<List<List<Integer>>> data;
+    private List<Integer> flatData;
 
     public CaptureViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is 'Capturar' fragment");
+        data = new MutableLiveData<>();
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Handler handler = new Handler();
+
+        executor.execute(() -> {
+            List<List<Integer>> response = JsonDataService.getJsonData();
+            handler.post(() -> {
+                flatData = flatList(response);
+                data.setValue(response);
+            });
+        });
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public LiveData<List<List<Integer>>> getData() {
+        return data;
+    }
+
+    public String join() {
+        StringBuilder builder = new StringBuilder();
+        flatData.forEach(builder::append);
+        return builder.toString();
+    }
+
+    public Integer major() {
+        return flatData.stream().mapToInt(i -> i).max().orElse(0);
+    }
+
+    public Double average() {
+        return flatData.stream().mapToInt(i -> i).average().orElse(0);
+    }
+
+    public List<Integer> descending() {
+        List<Integer> list = flatData.stream().sorted().collect(Collectors.toList());
+        ArrayList<Integer> revertedList = new ArrayList<>();
+
+        for (int i = list.size() - 1; i >= 0; i--)
+            revertedList.add(list.get(i));
+
+        return revertedList;
+    }
+
+    private List<Integer> flatList(List<List<Integer>> list) {
+        List<Integer> flatList = new ArrayList<>();
+
+        for (List<Integer> l : list)
+            flatList.addAll(l);
+
+        return flatList;
     }
 }
